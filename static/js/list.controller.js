3de388349d2130405ffe33631d945d7e375308c6
Mouse Reeve <mouse.reeve@gmail.com>
2015-11-17 20:21:53 -0800

    Sortable tables

--- static/js/list.controller.js
+++ static/js/list.controller.js
@@ -2,12 +2,23 @@ angular.module('app').controller('ListCtrl', ['$routeParams', '$scope', 'Grimoir
         function($routeParams, $scope, Grimoire) {
 
     $scope.type = $routeParams.type || 'grimoires';
+    $scope.sortField = 'properties.title';
+    $scope.reverse = false;
 
     var loadData = function () {
         Grimoire.loadList($scope.type).then(function (data) {
             $scope.nodes = data.nodes;
             $scope.properties = data.properties;
         });
+    };
+
+    $scope.setSort = function (field) {
+        if ($scope.sortField == 'properties.' + field) {
+            $scope.reverse = !$scope.reverse;
+        } else {
+            $scope.sortField = 'properties.' + field;
+            $scope.reverse = false;
+        }
     };
 
     $scope.edit = function (node) {
